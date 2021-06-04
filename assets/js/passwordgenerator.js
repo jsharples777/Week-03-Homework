227e0dc4dbaec352f5b7192e524cfe791337f2b8
Jim Sharples <jamie.sharples@gmail.com>
2021-06-04 16:20:27 +1000

    First upload

---
+++ assets/js/passwordgenerator.js
@@ -0,0 +1,86 @@
+class PasswordGenerator {
+    constructor() {
+        this.lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+        this.upperCaseAlphabet = "ABCDEDFGHIJKLMNOPQRSTUVWXYZ";
+        this.digits = "0123456789";
+        this.symbols = "!@#$%^&*()-+=[{]}\|;:<,>.?/`~";
+    }
+
+    generateItemUsingString(source) {
+        let characterIndex = Math.floor(Math.random() * source.length);
+        return source[characterIndex];
+    }
+
+    generateLowerCaseCharacter() {
+        return this.generateItemUsingString(this.lowerCaseAlphabet);
+    }
+
+    generateUpperCaseCharacter() {
+        return this.generateItemUsingString(this.upperCaseAlphabet);
+    }
+
+    generateDigit() {
+        return this.generateItemUsingString(this.digits);
+    }
+
+    generateSymbol() {
+        return this.generateItemUsingString(this.symbols);
+    }
+
+    generateCharacter (useLowerCaseAlphabet,useUpperCaseAlphabet,useDigits,useSymbols) {
+        let numberOfOptionsForCharacter = 0;
+
+        let options = [0,0,0,0]; // the four options are off initially;
+
+        if (useLowerCaseAlphabet) {
+            options[numberOfOptionsForCharacter] = 1; // 1 is lowercase
+            numberOfOptionsForCharacter++;
+        }
+        if (useUpperCaseAlphabet) {
+            options[numberOfOptionsForCharacter] = 2; // 2 is uppercase
+            numberOfOptionsForCharacter++;
+        }
+
+        if (useDigits) {
+            options[numberOfOptionsForCharacter] = 3; // 3 is numeric
+            numberOfOptionsForCharacter++;
+        }
+
+        if (useSymbols) {
+            options[numberOfOptionsForCharacter] = 4; // 4 is symbol
+            numberOfOptionsForCharacter++;
+        }
+
+        let randomOptionChosen = Math.floor(Math.random() * numberOfOptionsForCharacter);
+        let chosenCharacterGenerator = options[randomOptionChosen];
+
+        switch(chosenCharacterGenerator) {
+            case 1 : return this.generateLowerCaseCharacter();
+            case 2 : return this.generateUpperCaseCharacter();
+            case 3 : return this.generateDigit();
+            case 4 : return this.generateSymbol();
+        }
+
+    }
+
+    generate (length,useLowerCaseAlphabet,useUpperCaseAlphabet,useDigits,useSymbols) {
+        if (useLowerCaseAlphabet === undefined) {
+            useLowerCaseAlphabet = true;
+        }
+        if (useUpperCaseAlphabet === undefined) {
+            useUpperCaseAlphabet = false;
+        }
+        if (useDigits === undefined) {
+            useDigits = true;
+        }
+        if (useSymbols === undefined) {
+            useSymbols = false;
+        }
+
+        let newPassword = "";
+        for (let index = 0; index < length;index++) {
+            newPassword += this.generateCharacter(useLowerCaseAlphabet,useUpperCaseAlphabet,useDigits,useSymbols);
+        }
+        return newPassword;
+    }
+}
---
+++ assets/js/script.js
@@ -0,0 +1,79 @@
+/* simple debugging code using console log */
+let debug = false;
+
+let log = function(message) {
+  if (debug) {
+    this.console.log(message);
+  }
+}
+
+
+log("Started Event Handlers")
+
+/* setup the display of the slider value for the password length */
+let passwordLengthElement = this.document.getElementById("pLength");
+let displaySpanElement = this.document.getElementById("pLengthDisplay");
+
+passwordLengthElement.value="8"; // set initial value of the slider for password length;
+
+let pLengthDisplayHandler = function(event) {
+  log("Changed value to " + passwordLengthElement.value);
+  displaySpanElement.innerText = passwordLengthElement.value;
+}
+
+passwordLengthElement.onchange = pLengthDisplayHandler;
+passwordLengthElement.oninput = pLengthDisplayHandler;
+
+/* setup the validation of the checkboxes to ensure at least is selected */
+let useLowerCaseElement = this.document.getElementById("useLowerCase");
+let useUpperCaseElement = this.document.getElementById("useUpperCase");
+let useNumericElement = this.document.getElementById("useNumeric");
+let useSymbolElement = this.document.getElementById("useSymbol");
+
+let generateButtonElement = this.document.getElementById("generate");
+
+let checkValidationHandler = function(event) {
+  let isLowerCaseChecked = useLowerCaseElement.checked;
+  let isUpperCaseChecked = useUpperCaseElement.checked;
+  let isNumericChecked = useNumericElement.checked;
+  let isSymbolChecked = useSymbolElement.checked;
+
+  if ((!isLowerCaseChecked) &&
+      (!isUpperCaseChecked) &&
+      (!isNumericChecked) &&
+      (!isSymbolChecked)) {
+        generateButtonElement.disabled = true;
+        log("Validation Handler: no checkbox selected");
+    }
+    else {
+      generateButtonElement.disabled = false;
+      log("Validation Handler: at least one checkboxes selected");
+    }
+}
+
+useLowerCaseElement.onchange = checkValidationHandler;
+useUpperCaseElement.onchange = checkValidationHandler;
+useNumericElement.onchange = checkValidationHandler;
+useSymbolElement.onchange = checkValidationHandler;
+
+/*
+  When the button is clicked, generate the password and display it
+*/
+let passwordElement = this.document.getElementById("password");
+let generator = new PasswordGenerator();
+
+let generatePasswordHandler = function(event) {
+  // collect the values for the password generation
+  let passwordLength = passwordLengthElement.value;
+  let useLowerCase = useLowerCaseElement.checked;
+  let useUpperCase = useUpperCaseElement.checked;
+  let useNumeric = useNumericElement.checked;
+  let useSymbol = useSymbolElement.checked;
+
+  
+  let password = generator.generate(passwordLength,useLowerCase,useUpperCase,useNumeric,useSymbol);
+  log("Password has length " + password.length + " and is " + password);
+  passwordElement.value = password;
+}
+
+generateButtonElement.onclick = generatePasswordHandler;
